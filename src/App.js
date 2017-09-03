365ac41b304c70cb440f315f66927b46a215b014
rhyso <rhyswork@gmail.com>
2017-09-03 18:18:53 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+
+    render() {
+
+
+    return (
+      <div className="App">
+
+
+          <div className="container">
+              <a href="mailto:rhyswork@gmail.com"><img src="mb7.png" title="bemorejosh" alt="bemorejosh" /></a>
+          </div>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
